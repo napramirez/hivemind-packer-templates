a316dc715544b146b40827f76995c05cefe67a32
Nap Ramirez <napramirez@gmail.com>
2015-09-25 11:55:40 -0400

    Added the apt cache in the clean up

--- ubuntu-14.04.3-server-amd64/scripts/cleanup.sh
+++ ubuntu-14.04.3-server-amd64/scripts/cleanup.sh
@@ -3,6 +3,9 @@
 # Delete unneeded files.
 #rm -f /home/vagrant/*.sh
 
+# Clear the package cache
+apt-get clean
+
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
